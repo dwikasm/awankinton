5a801b39904e83d0e7a8cfeb891d576ef6458315
dwikasm <dwikasm@gmail.com>
2017-03-09 21:05:31 +0700

    exercise 3 clear

---
+++ vagrant-exercise3/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y software-properties-common python-software-properties
+apt-add-repository ppa:ondrej/php -y
+apt-get update
+apt-get install -y --force-yes apache2
+apt-get install -y --force-yes php7.0-common php7.0 php7.0-zip
+
