523378d1fcbdc755e81403fabd16579a776da60f
adisaptorenggo <saptorenggoadi@yahoo.co.id>
2017-03-16 00:03:05 +0700

    ngehapus double file

--- vagrant-exercise1/bootstrap.sh
+++ vagrant-exercise1/bootstrap.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get install -y software-properties-common python-software-properties
 apt-add-repository ppa:ondrej/php -y
 apt-get update
-apt-get install -y --force-yes apache2
-apt-get install -y --force-yes php7.0-common php7.0-opcache php7.0 php7.0-zip
+apt-get install -y apache2
+apt-get install -y php7.0-common php7.0-opcache php7.0 php7.0-zip
 
